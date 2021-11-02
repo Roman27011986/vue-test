23f131497d82b0ac21c04369453d30671dfac5fb
roman.nesterenko@siz.bz <didorsn@gmail.com>
2021-11-02 16:44:09 +0200

    qwe

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div id='app'>
+  <Btn>Roma</Btn>
+</div>
+</template>
+
+<script>
+import Btn from './components/Button.vue'
+  export default {
+    name:'App',
+    components:{
+      Btn
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+    <button v-bind:type="typeBtn" :class="{ btn:true, 'btn--outlined':outlined }">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+      name:'Btn',
+      props:{
+          typeBtn:{
+              type:String,
+              default:'button'
+          },
+          outlined:{
+              type:Boolean,
+              default:false
+          }
+      }  
+    }
+</script>
+
+<style lang="scss" scoped>
+   .btn{
+       display: inline-block;
+       border: none;
+       background-color: #FF662D;
+       color: #fff;
+       cursor: pointer;
+
+       &--outlined{
+           background: none;
+           border: 1px solid #FF662D;
+           color: black;
+       }
+   }
+</style>
