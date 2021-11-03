57d48fac6db2dfeb4ce03e09a0bb5e7e12327962
roman.nesterenko@siz.bz <didorsn@gmail.com>
2021-11-03 10:55:40 +0200

    qwe

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
 <div id='app'>
   <Btn>Roma</Btn>
+  <StarRating :rating="2.5"/>
 </div>
 </template>
 
 <script>
+import StarRating from './components/StarRating.vue'
 import Btn from './components/Button.vue'
   export default {
     name:'App',
     components:{
-      Btn
+      Btn,
+      StarRating
     }
   }
 </script>
---
+++ src/components/StarRating.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="star-rating">
+      <span v-for="idx in starLimit" :key="idx" class="star star--outlined"></span>
+      <div class="star-rating__fullfilled" :style="`width:${ratingWidth}`">
+          <span v-for="idx in starLimit" :key="idx" class="star star--colored"></span>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'StarRating',
+        props:{
+            rating:{
+                type:Number,
+                default:0
+            },
+            starLimit:{
+                type:Number,
+                default:5
+            }
+        },
+        computed:{
+            ratingWidth(){
+                return this.rating / this.starLimit * 100 + '%'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+ .star-rating {
+     position: relative;
+     display: inline-flex;
+     &__fullfilled {
+         display: inline-flex;
+         position: absolute;
+         width: 0;
+         height: 100%;
+         top: 0;
+         left: 0;
+         overflow: hidden;
+     }
+ }
+  .star {
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      margin: 0 3px;
+      flex-shrink: 0;
+      &--colored{
+          background: url('../assets/img/star.svg') center no-repeat;
+      }
+      &--outlined{
+          background: url('../assets/img/star-outlined.svg') center no-repeat;
+      }
+  }
+</style>
